f06372f5ecf64e858a4dca2b2c272724095a9359
minhmax098 <hocminh1998@gmail.com>
2020-05-29 23:30:11 +0700

    first commit

---
+++ tms/includes/footer.php
@@ -0,0 +1,39 @@
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<footer class="site-footer wow fadeInDown animated animated">
+      <div class="container">
+		<div class="row">
+			<div class="col-md-6 col-lg-6 col-xl-6 mx-auto mt-6">
+				<h4>About</h4>
+				<p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+			</div>
+
+
+			<!-- Grid column -->
+			<div class="col-md-6 col-lg-6 col-xl-6 mx-auto mt-6">
+				<h4>Contact</h4>
+				<p><i class="fa fa-home mr-3"></i> New York, NY 10012, US</p>
+				<p><i class="fa fa-envelope mr-3"></i> info@gmail.com</p>
+				<p><i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+				<p><i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
+			</div>
+		<!-- Grid column -->
+		</div>
+		<hr>
+      </div>
+
+		<div class="container">
+			<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+				<ul>
+					<li><a class="facebook" href="#"><span>Facebook</span></a></li>
+					<li><a class="twitter" href="#"><span>Twitter</span></a></li>
+					<li><a class="flickr" href="#"><span>Flickr</span></a></li>
+					<li><a class="googleplus" href="#"><span>Google+</span></a></li>
+					<li><a class="dribbble" href="#"><span>Dribbble</span></a></li>
+				</ul>
+			</div>
+			<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2017 TMS . All Rights Reserved </p>
+		</div>
+	</footer>
+</div>
